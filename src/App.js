23e718789a32bd001dfc386c372ec19962286b96
Achref Soltani <78027438+wxcv-ai@users.noreply.github.com>
2023-04-02 05:10:30 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from "./pages/signIn.js"
+import Register from "./pages/register.js"
+
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
