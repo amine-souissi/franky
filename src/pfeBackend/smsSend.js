11154fcd58e5c6df42d2e1b0d0a41ed9c8b71902
amine-souissi <116435946+amine-souissi@users.noreply.github.com>
2023-04-13 00:32:11 +0200

    Update smsSend.js

--- src/pfeBackend/smsSend.js
+++ src/pfeBackend/smsSend.js
@@ -6,7 +6,7 @@ async function  getSms() {
 	var bodyParameters =  {
 	'type': '55',
 	'sender': "TunSMS Test",
-	'sms':[{'mobile':'56212075','sms':'testSend2'}]}
+	'sms':[{'mobile':'20934401','sms':'testSend2'}]}
 
 	var token = "12e!JhHfn8UKIKCmZb2qVcSFpI=vCL6jomk8V2pICKeVgsIGUM70!bHqvIl9futUheeDiTXwpCSW5moc9xeAax1WGohxjbSRG6g!cE55"
 	var config = {
